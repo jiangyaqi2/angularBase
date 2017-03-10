92110005827a5f0dcdec90733d80d01e840f7251
jiangyaqi2 <525168624@qq.com>
2017-03-10 22:41:24 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path: '', component: DemoComponent},
+      {path: 'demo', component: DemoComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
